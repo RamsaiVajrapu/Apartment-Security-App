2f8315c3e424bbcd9e31ef4101627b27d2ba48bb
Ram Sai Vajrapu <Ram Sai Vajrapu@RAMSAI-VAJRAPU>
2021-03-19 12:28:01 +0530

    ...

--- src/main/java/com/cg/aps/application/ApartmentSecurityAppApplication.java
+++ src/main/java/com/cg/aps/application/ApartmentSecurityAppApplication.java
@@ -2,8 +2,12 @@ package com.cg.aps.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.cg.aps")
+@EntityScan(basePackages = "com.cg.aps.entity")
+@EnableJpaRepositories(basePackages = "com.cg.aps.dao")
 public class ApartmentSecurityAppApplication {
 
 	public static void main(String[] args) {
